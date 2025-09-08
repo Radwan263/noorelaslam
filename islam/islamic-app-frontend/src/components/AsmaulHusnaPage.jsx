3dde09da43a36d1cbc6b9705ef50b35d060e0786
Radwan263 <Rowanradwan263@gmail.com>
2025-09-08 14:48:01 +0300

    Create AsmaulHusnaPage.jsx

---
+++ islam/islamic-app-frontend/src/components/AsmaulHusnaPage.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const AsmaulHusnaPage = () => ( <div><h1 style={{color: 'white', textAlign: 'center', fontFamily: 'Amiri'}}>سيتم بناء قسم أسماء الله الحسنى هنا</h1></div> );
+export default AsmaulHusnaPage;
