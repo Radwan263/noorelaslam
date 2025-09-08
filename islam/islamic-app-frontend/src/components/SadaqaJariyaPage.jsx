37f09f24e35ae744a4441a9fa2e35882b7e61026
Radwan263 <Rowanradwan263@gmail.com>
2025-09-08 14:48:37 +0300

    Create SadaqaJariyaPage.jsx

---
+++ islam/islamic-app-frontend/src/components/SadaqaJariyaPage.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const SadaqaJariyaPage = () => ( <div><h1 style={{color: 'white', textAlign: 'center', fontFamily: 'Amiri'}}>سيتم بناء قسم الصدقة الجارية هنا</h1></div> );
+export default SadaqaJariyaPage;
