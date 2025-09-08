fb324e9754c7da95be79e935b0209546040b87a5
Radwan263 <Rowanradwan263@gmail.com>
2025-09-08 14:09:40 +0300

    Update AzkarDisplayPage.jsx

--- islam/islamic-app-frontend/src/components/AzkarDisplayPage.jsx
+++ islam/islamic-app-frontend/src/components/AzkarDisplayPage.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react';
+
+const AzkarDisplayPage = () => {
+  return (
+    <div>
+      {/* سيتم عرض الأذكار هنا قريبًا */}
+    </div>
+  );
+};
+
+export default AzkarDisplayPage; // <-- وهذا أيضًا
