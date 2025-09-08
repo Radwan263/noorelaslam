a5cf63cbff573d0612a5f6005236cdc1c1607e99
Radwan263 <Rowanradwan263@gmail.com>
2025-09-08 14:47:17 +0300

    Create DuasPage.jsx

---
+++ islam/islamic-app-frontend/src/components/DuasPage.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const DuasPage = () => ( <div><h1 style={{color: 'white', textAlign: 'center', fontFamily: 'Amiri'}}>سيتم بناء قسم الأدعية هنا</h1></div> );
+export default DuasPage;
