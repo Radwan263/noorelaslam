71b2f88805c3515b33aa44f0bf47822c91c19d66
Radwan263 <Rowanradwan263@gmail.com>
2025-09-09 03:22:59 +0300

    Update App.jsx

--- islam/islamic-app-frontend/src/App.jsx
+++ islam/islamic-app-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuranSection from './components/QuranSection';
 import SurahPage from './components/SurahPage';
@@ -13,36 +13,23 @@ import SadaqaJariyaPage from './components/SadaqaJariyaPage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 
-// --- مكون جديد لإدارة الـ "key" ---
-// هذا المكون سيقوم باستخراج المسار الحالي واستخدامه كـ "key"
-// مما يجبر React على إعادة تحميل الصفحة عند تغيير المسار
-const AnimatedRoutes = () => {
-  const location = useLocation();
-
-  return (
-    <Routes location={location} key={location.pathname}>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/quran" element={<QuranSection />} />
-      {/* 👇 السطر الأهم: لا نحتاج لإضافة key هنا لأننا أضفناه في الأعلى 👇 */}
-      <Route path="/quran/:surahNumber" element={<SurahPage />} />
-      <Route path="/hadith" element={<HadithCollectionsPage />} />
-      <Route path="/hadith/:collectionName" element={<HadithListPage />} />
-      <Route path="/azkar" element={<AzkarCategoriesPage />} />
-      <Route path="/azkar/:category" element={<AzkarDisplayPage />} />
-      <Route path="/duas" element={<DuasPage />} />
-      <Route path="/asma-ul-husna" element={<AsmaulHusnaPage />} />
-      <Route path="/sadaqa-jariya" element={<SadaqaJariyaPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
-  );
-};
-
-
 function App() {
   return (
     <Router>
-      <AnimatedRoutes />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quran" element={<QuranSection />} />
+        <Route path="/quran/:surahNumber" element={<SurahPage />} />
+        <Route path="/hadith" element={<HadithCollectionsPage />} />
+        <Route path="/hadith/:collectionName" element={<HadithListPage />} />
+        <Route path="/azkar" element={<AzkarCategoriesPage />} />
+        <Route path="/azkar/:category" element={<AzkarDisplayPage />} />
+        <Route path="/duas" element={<DuasPage />} />
+        <Route path="/asma-ul-husna" element={<AsmaulHusnaPage />} />
+        <Route path="/sadaqa-jariya" element={<SadaqaJariyaPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </Router>
   );
 }
