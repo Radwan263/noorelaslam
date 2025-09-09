111222ba31b151d8701f0a430e99dc131722bc3f
Radwan263 <Rowanradwan263@gmail.com>
2025-09-09 04:09:18 +0300

    Update App.jsx

--- islam/islamic-app-frontend/src/App.jsx
+++ islam/islamic-app-frontend/src/App.jsx
@@ -1,34 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import QuranSection from './components/QuranSection';
-import SurahPage from './components/SurahPage';
-import HadithCollectionsPage from './components/HadithCollectionsPage';
-import HadithListPage from './components/HadithListPage';
-import AzkarCategoriesPage from './components/AzkarCategoriesPage';
-import AzkarDisplayPage from './components/AzkarDisplayPage';
+// ... (باقي الاستيرادات)
+import DuasCategoriesPage from './components/DuasCategoriesPage'; // إضافة جديدة
 import DuasPage from './components/DuasPage';
-import AsmaulHusnaPage from './components/AsmaulHusnaPage';
-import SadaqaJariyaPage from './components/SadaqaJariyaPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quran" element={<QuranSection />} />
-        <Route path="/quran/:surahNumber" element={<SurahPage />} />
-        <Route path="/hadith" element={<HadithCollectionsPage />} />
-        <Route path="/hadith/:collectionName" element={<HadithListPage />} />
-        <Route path="/azkar" element={<AzkarCategoriesPage />} />
-        <Route path="/azkar/:category" element={<AzkarDisplayPage />} />
-        <Route path="/duas" element={<DuasPage />} />
-        <Route path="/asma-ul-husna" element={<AsmaulHusnaPage />} />
-        <Route path="/sadaqa-jariya" element={<SadaqaJariyaPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        {/* ... (باقي المسارات) */}
+        <Route path="/duas" element={<DuasCategoriesPage />} /> {/* تعديل */}
+        <Route path="/duas/:categoryId" element={<DuasPage />} /> {/* تعديل */}
+        {/* ... (باقي المسارات) */}
       </Routes>
     </Router>
   );
